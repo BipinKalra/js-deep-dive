ab7c02d38bed0363401df2e8c9ae6f1322e09db6
BipinKalra <bipinkalra@gmail.com>
2021-02-22 01:08:54 +0530

    changes

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 200 * numPassengers // Parameters defined before it can be used in the expression
+) {
+  // Pre ES6 code
+  // numPassengers = numPassengers || 1;
+  // price = price || 200;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("BK123");
+createBooking("BK123", 9);
