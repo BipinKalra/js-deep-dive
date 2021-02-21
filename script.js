84a77f193b90c19c1215ef8a3a7542fe1e859b0e
BipinKalra <bipinkalra@gmail.com>
2021-02-22 01:28:15 +0530

    objects vs primitives

--- script.js
+++ script.js
@@ -23,3 +23,29 @@ const createBooking = function (
 
 createBooking("BK123");
 createBooking("BK123", 9);
+
+const flight = "LH234";
+const bipin = {
+  name: "Bipin Kalra",
+  passport: "1234567",
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH690";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 1234567) {
+    console.log("Checked In!");
+  } else {
+    console.log("Wrong Passport!");
+  }
+};
+
+checkIn(flight, bipin);
+console.log(flight);
+console.log(bipin);
+// As we can see above, by running the function, the flight number doesnt change
+// But the Name inside object does change
+// This is because primitives are passed to functions as copies
+// flightNum = flight
+// On the other hand, objects are passed by references
